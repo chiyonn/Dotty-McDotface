444f2dac02327e1980d44e5095cf6f84ab17abff
chiyonn <kud0h.chi4@gmail.com>
2025-02-13 20:15:25 +0900

    clean: no need to see tips

--- git_sync_check.sh
+++ git_sync_check.sh
@@ -5,13 +5,4 @@ HERE=$(cd $(dirname $0); pwd)
 if test -n "$(git -C ${HERE} status --porcelain)" ||
    ! git -C ${HERE} diff --exit-code --stat --cached origin/main > /dev/null ; then
   echo -e "\e[36m=== DOTFILES IS DIRTY ===\e[m"
-  echo -e "\e[33mThe dotfiles have been changed.\e[m"
-  echo -e "\e[33mPlease update them with the following command.\e[m"
-  echo "  cd ${HERE}"
-  echo "  git add ."
-  echo "  git commit -m \"update dotfiles\""
-  echo "  git push origin main"
-  echo -e "\e[33mor\e[m"
-  echo "  git push origin main"
-  echo -e "\e[36m=========================\e[m"
 fi
